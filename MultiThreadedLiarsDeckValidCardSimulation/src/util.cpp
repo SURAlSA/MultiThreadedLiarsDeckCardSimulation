bcf2501722cbef2bca57141e12764085a435fc26
Zane <145461046+SURAlSA@users.noreply.github.com>
2025-03-10 20:24:55 +1100

    Saw a video online in algorithms which said not to create a new random device each time a random integer is created

--- MultiThreadedLiarsDeckValidCardSimulation/src/util.cpp
+++ MultiThreadedLiarsDeckValidCardSimulation/src/util.cpp
@@ -1,12 +1,11 @@
 #include "util.h"
 
 // functions
+random_device rd;
+mt19937 generator(rd());
 
 int getRandomInt(int min, int max)
 {
-    random_device rd;
-    mt19937 generator(rd());
-
     uniform_int_distribution<int> distribution(min, max);
 
     return distribution(generator);
